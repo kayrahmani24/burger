47e4caa5a7eec259eda45a691c57b7bf5d23c50b
Kayvan Rahmani <Rahmani.kay1@gmail.com>
2017-12-18 11:08:50 -0600

    backend setup

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
+
+
+//Npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var PORT = process.env.PORT || 8080;
+
 
 var app = express();
 
